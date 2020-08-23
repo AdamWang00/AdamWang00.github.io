1e42ef4cd2908dbb7eee9ad291c271503d9e4257
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-08-23 04:25:24 -0400

    start projects

--- script.js
+++ script.js
@@ -3,25 +3,40 @@ let modal;
 window.addEventListener('load', (event) => {
     modal = document.getElementById("modal");
 
-    fadeIn("fade-1", 1000);
-    setTimeout(() => fadeIn("fade-2", 600), 2000);
-    setTimeout(() => fadeIn("fade-3", 600), 2400);
-    setTimeout(() => fadeIn("fade-4", 600), 2800);
-    setTimeout(() => fadeIn("fade-5", 600), 3200);
-});
+    const highlight = e => e.classList.add("highlight");
 
-const fadeIn = (id, highlightDelay) => {
-    document.getElementById(id).style.opacity = 1.0;
-    document.querySelectorAll(`#${id} .mark`).forEach(e =>
-        setTimeout(() => e.style.color = "orange", highlightDelay));
-}
+    fadeIn("fade-1", 1000, highlight);
+    setTimeout(() => fadeIn("fade-2", 600, highlight), 2000);
+    setTimeout(() => fadeIn("fade-3", 600, highlight), 2400);
+    setTimeout(() => fadeIn("fade-4", 600, highlight), 2800);
+    setTimeout(() => fadeIn("fade-5", 600, highlight), 3200);
+});
 
 const showModal = () => {
     modal.style.top = 0;
     modal.style.opacity = 0.95;
-}
+
+    const show = id => document.getElementById(id).style.opacity = 1.0;
+
+    setTimeout(() => show("project-1"), 800);
+    setTimeout(() => show("project-2"), 1000);
+    setTimeout(() => show("project-3"), 1200);
+    setTimeout(() => show("project-4"), 1400);
+};
 
 const hideModal = () => {
-    modal.style.top = "-100vh"
+    modal.style.top = "-100vh";
     modal.style.opacity = 0;
-}
+
+    const projects = [1, 2, 3, 4];
+    projects.forEach(n => {
+        document.getElementById(`project-${n}`).style.opacity = 0;
+    });
+};
+
+const fadeIn = (id, delay, func) => {
+    document.getElementById(id).style.opacity = 1.0;
+    document.querySelectorAll(`#${id} .mark`).forEach(e => {
+        setTimeout(() => func(e), delay);
+    });
+};
